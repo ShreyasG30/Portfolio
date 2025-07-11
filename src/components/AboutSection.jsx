fa95284e1ac004f6e5c6f261d63b73b22846c41b
Ruthvik K S <rk99219@gmail.com>
2025-07-11 21:52:00 +0530

    Fixed download CV

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -31,7 +31,7 @@ export const AboutSection = () => {
                 Get In Touch
               </a>
               <a
-                href="/docs/Shreyas_Girish.pdf" download="Shreyas_Girish_CV.pdf"
+                href="/Portfolio/docs/Shreyas_Girish.pdf" download="Shreyas_Girish_CV.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300 w-full sm:w-auto text-center"
               >
                 Download CV
